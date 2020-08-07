9ef5725d8415eec32fc8bf8a22bc466babae981f
Lucas Salvador <lucas.salvador11358@gmail.com>
2020-08-06 22:09:19 -0300

    adicionando table component

--- client/src/App.vue
+++ client/src/App.vue
@@ -44,6 +44,16 @@
           xs="12"
           sm="12"
         >
+          <Table
+            :items="desserts"
+            :headers="headers"
+          />
+        </v-col>
+        <v-col
+          cols="12"
+          xs="12"
+          sm="12"
+        >
            <ListChart
               :chart-data="datacollection"
               :height="130"
@@ -60,19 +70,117 @@
 </template>
 
 <script>
-//  import HelloWorld from './components/HelloWorld'
-import ListChart from './components/ListChart'
+import ListChart from './components/ListChart/ListChart'
+import Table from './components/Table/Table'
 
 export default {
   name: 'App',
 
   components: {
-    // HelloWorld,
-    ListChart
+    ListChart,
+    Table
   },
 
   data: () => ({
-    datacollection: null
+    datacollection: null,
+    headers: [
+      {
+        text: 'Dessert (100g serving)',
+        align: 'start',
+        value: 'name'
+      },
+      {
+        text: 'Calories',
+        value: 'calories',
+        sortable: false
+      },
+      { text: 'Fat (g)', value: 'fat', sortable: false },
+      { text: 'Carbs (g)', value: 'carbs', sortable: false },
+      { text: 'Protein (g)', value: 'protein', sortable: false },
+      { text: 'Iron (%)', value: 'iron', sortable: false }
+    ],
+    desserts: [
+      {
+        name: 'Frozen Yogurt',
+        calories: 159,
+        fat: 6.0,
+        carbs: 24,
+        protein: 4.0,
+        iron: '1%'
+      },
+      {
+        name: 'Ice cream sandwich',
+        calories: 237,
+        fat: 9.0,
+        carbs: 37,
+        protein: 4.3,
+        iron: '1%'
+      },
+      {
+        name: 'Eclair',
+        calories: 262,
+        fat: 16.0,
+        carbs: 23,
+        protein: 6.0,
+        iron: '7%'
+      },
+      {
+        name: 'Cupcake',
+        calories: 305,
+        fat: 3.7,
+        carbs: 67,
+        protein: 4.3,
+        iron: '8%'
+      },
+      {
+        name: 'Gingerbread',
+        calories: 356,
+        fat: 16.0,
+        carbs: 49,
+        protein: 3.9,
+        iron: '16%'
+      },
+      {
+        name: 'Jelly bean',
+        calories: 375,
+        fat: 0.0,
+        carbs: 94,
+        protein: 0.0,
+        iron: '0%'
+      },
+      {
+        name: 'Lollipop',
+        calories: 392,
+        fat: 0.2,
+        carbs: 98,
+        protein: 0,
+        iron: '2%'
+      },
+      {
+        name: 'Honeycomb',
+        calories: 408,
+        fat: 3.2,
+        carbs: 87,
+        protein: 6.5,
+        iron: '45%'
+      },
+      {
+        name: 'Donut',
+        calories: 452,
+        fat: 25.0,
+        carbs: 51,
+        protein: 4.9,
+        iron: '22%'
+      },
+      {
+        name: 'KitKat',
+        calories: 518,
+        fat: 26.0,
+        carbs: 65,
+        protein: 7,
+        iron: '6%'
+      }
+    ]
   }),
   mouted () {
     this.fillData()
---
+++ client/src/components/Table/Table.vue
@@ -0,0 +1,31 @@
+<template>
+   <v-data-table
+    :headers="headers"
+    :items="items"
+    :items-per-page="itemsPerPage ? itemsPerPage : 5"
+    class="elevation-1"
+  ></v-data-table>
+</template>
+
+<script>
+export default {
+  props: {
+    headers: {
+      type: Array,
+      required: true
+    },
+    items: {
+      type: Array,
+      required: true
+    },
+    itemsPerPage: {
+      type: Number,
+      default: 5
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
