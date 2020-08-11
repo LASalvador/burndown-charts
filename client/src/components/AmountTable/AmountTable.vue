a83c1b176466bf62cc9087bc58c00b8d6685b498
Lucas Salvador <lucas.salvador11358@gmail.com>
2020-08-10 22:09:50 -0300

    adicionando tabela de quantidade de tarefas

---
+++ client/src/components/AmountTable/AmountTable.vue
@@ -0,0 +1,44 @@
+<template>
+   <v-data-table
+    :headers="headers"
+    :items="items"
+    :items-per-page="itemsPerPage ? itemsPerPage : 5"
+    class="elevation-1"
+  >
+  <template v-slot:item.plan="{ item }">
+    <v-text-field
+      label="Planejado"
+      v-model="item.plan"
+    ></v-text-field>
+  </template>
+  <template v-slot:item.actual="{ item }">
+    <v-text-field
+      label="Atual"
+      v-model="item.actual"
+    ></v-text-field>
+  </template>
+  </v-data-table>
+</template>
+
+<script>
+export default {
+  props: {
+    headers: {
+      type: Array,
+      required: true
+    },
+    items: {
+      type: Array,
+      required: true
+    },
+    itemsPerPage: {
+      type: Number,
+      default: 5
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
